50eabad6dcadaedc3f59d5074209ee3cc2305e4b
Jona <jonathan.raphael@gmail.com>
2020-04-04 01:45:29 -0400

    Added DB row jsons

--- SciHub_class.py
+++ SciHub_class.py
@@ -1,12 +1,15 @@
 # %%
 import os
 import xmltodict
+from datetime import datetime
 
 class Sho:
     def __init__(self, grd_id, user="jonaraph", pwd="fjjEwvMDHyJH9Fa"):
         self.grd_id = grd_id
         self.user = user
         self.pwd = pwd
+        self.isoceanic = None
+        self.oceanintersection = None
 
         self.generic_id = grd_id[:7]+"????_?"+grd_id[13:-4]+"*"
         self.URLs = {"query_prods" : f"https://scihub.copernicus.eu/apihub/search?rows=100&q=(platformname:Sentinel-1 AND filename:{self.generic_id})",}
@@ -40,7 +43,7 @@ class Sho:
         grd_vv = [e for e in self.query_grd_tiffs_res.get('feed').get('entry') if '-vv-' in e.get('title').get('#text')][0]
         self.grd_vv = grd_vv.get('title').get('#text')
 
-        self.URLs['download_grd_tiff'] = f"https://scihub.copernicus.eu/dhus/odata/v1/Products('{self.grd_shid}')/Nodes('{self.grd_id}.SAFE')/Nodes('measurement')/{self.grd_vv}/$value"
+        self.URLs['download_grd_tiff'] = f"https://scihub.copernicus.eu/dhus/odata/v1/Products('{self.grd_shid}')/Nodes('{self.grd_id}.SAFE')/Nodes('measurement')/Nodes('{self.grd_vv}')/$value"
         self.wgets['download_grd_tiff'] = f'wget --no-check-certificate --user={self.user} --password={self.pwd} --output-document=grd_vv.tiff "{self.URLs["download_grd_tiff"]}"'
 
     def __repr__(self):
@@ -50,6 +53,67 @@ class Sho:
     def download_ocn(self): return os.system(self.wgets["download_ocn"])
     def download_grd_tiff(self): return os.system(self.wgets["download_grd_tiff"])
 
+    def grd_db_row(self):
+        grd = [e for e in self.query_prods_res.get('feed').get('entry') if 'GRD' in e.get('title')][0]
+        res = {
+            "GRD_scihub_uuid" : self.grd_shid,
+            "GRD_scihub_identifier" : self.grd_id,
+            "summary" : grd.get('summary'),
+            "beginposition" : str_to_dt(xml_get(grd.get('date'),'beginposition')),
+            "endposition" : str_to_dt(xml_get(grd.get('date'), 'endposition')),
+            "ingestiondate" : str_to_dt(xml_get(grd.get('date'), 'ingestiondate')),
+            "missiondatatakeid" : int(xml_get(grd.get('int'), 'missiondatatakeid')),
+            "orbitnumber" : int(xml_get(grd.get('int'), 'orbitnumber')),
+            "lastorbitnumber" : int(xml_get(grd.get('int'), 'lastorbitnumber')),
+            "relativeorbitnumber" : int(xml_get(grd.get('int'), 'relativeorbitnumber')),
+            "lastrelativeorbitnumber" : int(xml_get(grd.get('int'), 'lastrelativeorbitnumber')),
+            "sensoroperationalmode" : xml_get(grd.get('str'), 'sensoroperationalmode'),
+            "swathidentifier" : xml_get(grd.get('str'), 'swathidentifier'),
+            "orbitdirection" : xml_get(grd.get('str'), 'orbitdirection'),
+            "producttype" : xml_get(grd.get('str'), 'producttype'),
+            "timeliness" : xml_get(grd.get('str'), 'timeliness'),
+            "platformname" : xml_get(grd.get('str'), 'platformname'),
+            "platformidentifier" : xml_get(grd.get('str'), 'platformidentifier'),
+            "instrumentname" : xml_get(grd.get('str'), 'instrumentname'),
+            "instrumentshortname" : xml_get(grd.get('str'), 'instrumentshortname'),
+            "filename" : xml_get(grd.get('str'), 'filename'),
+            "format" : xml_get(grd.get('str'), 'format'),
+            "productclass" : xml_get(grd.get('str'), 'productclass'),
+            "polarisationmode" : xml_get(grd.get('str'), 'polarisationmode'),
+            "acquisitiontype" : xml_get(grd.get('str'), 'acquisitiontype'),
+            "status" : xml_get(grd.get('str'), 'status'),
+            "size" : xml_get(grd.get('str'), 'size'),
+            "footprint" : xml_get(grd.get('str'), 'footprint'),
+            "isoceanic" : self.isoceanic,
+            "oceanintersection" : self.oceanintersection, 
+            "timestamp" : str_to_dt(xml_get(grd.get('date'),'beginposition')).timestamp(),
+        }
+        return res
+
+    def ocn_db_row(self):
+        ocn = [e for e in self.query_prods_res.get('feed').get('entry') if 'OCN' in e.get('title')][0]
+        res = {
+            "OCN_scihub_uuid" : self.ocn_shid,
+            "OCN_scihub_identifier" : self.ocn_id,
+            "producttype" : xml_get(ocn.get('str'), 'producttype'),
+            "filename" : xml_get(ocn.get('str'), 'filename'),
+            "size" : xml_get(ocn.get('str'), 'size'),
+            "GRD_scihub_uuid" : self.grd_shid,
+        }
+        return res
+
+def xml_get(lst, a, key1="@name", key2="#text"):
+    # from a lst of dcts, find the dct that has key value pair (@name:a), then retrieve the value of (#text:?)
+    for dct in lst:
+        if dct.get(key1) == a:
+            return dct.get(key2)
+    return None
+
+def str_to_dt(s): return datetime.strptime(s[:-1], '%Y-%m-%dT%H:%M:%S.%f')
+
 a = Sho("S1B_IW_GRDH_1SDV_20200402T003114_20200402T003139_020958_027C13_47BB")
 a
+# %%
+a.download_grd_tiff()
+
 # %%
