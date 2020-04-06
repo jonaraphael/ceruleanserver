30f939cdb1c601ff67e43aa0e7b57f08e8ba92fc
Jona <jonathan.raphael@gmail.com>
2020-04-06 11:22:51 -0400

    Add local config on top of defaults

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
+from local_config import Local_Config
 class Config:
-    app_host = '0.0.0.0'
-    app_port = 80
-    app_debug = True
+    app_host = '0.0.0.0' if not hasattr(Local_Config, 'app_host') else Local_Config.app_host
+    app_port = 80 if not hasattr(Local_Config, 'app_port') else Local_Config.app_port
+    app_debug = True if not hasattr(Local_Config, 'app_debug') else Local_Config.app_debug
     
-    db_host = 'slick-db.cboaxrzskot9.eu-central-1.rds.amazonaws.com'
-    db_user = 'postgres'
-    db_password = 'postgres'
-    db_database = 'slick_db'
-    db_port = '5432'
+    db_host = 'slick-db.cboaxrzskot9.eu-central-1.rds.amazonaws.com' if not hasattr(Local_Config, 'db_host') else Local_Config.db_host
+    db_user = 'postgres' if not hasattr(Local_Config, 'db_user') else Local_Config.db_user
+    db_password = 'postgres' if not hasattr(Local_Config, 'db_password') else Local_Config.db_password
+    db_database = 'slick_db' if not hasattr(Local_Config, 'db_database') else Local_Config.db_database
+    db_port = '5432' if not hasattr(Local_Config, 'db_port') else Local_Config.db_port
 
-    sh_user = 'jonaraph'
-    sh_pwd = 'fjjEwvMDHyJH9Fa'
+    sh_user = 'jonaraph' if not hasattr(Local_Config, 'sh_user') else Local_Config.sh_user
+    sh_pwd = 'fjjEwvMDHyJH9Fa' if not hasattr(Local_Config, 'sh_pwd') else Local_Config.sh_pwd
+print(Config.__dict__)
---
+++ local_config.py
@@ -0,0 +1,6 @@
+class Local_Config:
+    app_host = 'localhost'
+    app_port = 9000
+
+    sh_user = 'jonaraph'
+    sh_pwd = 'fjjEwvMDHyJH9Fa'
