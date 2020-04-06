08bdc3491697fb7f0c93d8a7bfc407e60fab182e
Jona <jonathan.raphael@gmail.com>
2020-04-06 14:25:48 -0400

    switch from os.system to requests library

--- SciHub_class.py
+++ SciHub_class.py
@@ -4,30 +4,32 @@ from datetime import datetime
 import shapely.geometry as sh
 import json
 from config import Config
+import requests
 
 class SHO:
     def __init__(self, sns_msg, user=Config.sh_user, pwd=Config.sh_pwd):
         self.sns_msg = sns_msg
-        self.grd_id = self.sns_msg["id"]
         self.user = user
         self.pwd = pwd
+        self.grd_id = self.sns_msg["id"]
         self.dir = self.grd_id
         self.isoceanic = None
+        self.isvv = None
         self.oceanintersection = None
         self.grd = {}
         self.ocn = {}
         self.grd_shid = None
         self.ocn_id =  None
         self.ocn_shid =  None
+        self.ready = None
 
         self.generic_id = self.grd_id[:7]+"????_?"+self.grd_id[13:-4]+"*"
-        self.URLs = {"query_prods" : f"https://scihub.copernicus.eu/apihub/search?rows=100&q=(platformname:Sentinel-1 AND filename:{self.generic_id})",}
-        self.download_strs = {"query_prods" : f'wget --no-check-certificate --user={self.user} --password={self.pwd} --output-document={self.dir}/query_prods_results.xml "{self.URLs["query_prods"]}"',}
+        self.URLs = {"query_prods" : f"https://{self.user}:{self.pwd}@scihub.copernicus.eu/apihub/search?rows=100&q=(platformname:Sentinel-1 AND filename:{self.generic_id})",}
         
-        os.system(f'mkdir {self.dir}')
-        os.system(self.download_strs['query_prods'])
-        with open(f'{self.dir}/query_prods_results.xml') as f:
-            self.query_prods_res = xmltodict.parse(f.read())
+        with requests.Session() as s:
+            p = s.post(self.URLs["query_prods"])
+            self.query_prods_res = xmltodict.parse(p.text)
+        
         if self.query_prods_res.get('feed').get('opensearch:totalResults') == '0':
             print(f'ERROR No results found matching {self.grd_id}')
             return
@@ -42,16 +44,16 @@ class SHO:
         self.ocn_shid = self.ocn.get('id')
 
         if self.ocn:
-            self.URLs["download_ocn"] = f"https://scihubf.copernicus.eu/dhus/odata/v1/Products('{self.ocn_shid}')/%24value"
-            self.download_strs["download_ocn"] = f'wget --no-check-certificate --user={self.user} --password={self.pwd} --output-document={self.dir}/ocn.zip "{self.URLs["download_ocn"]}"'
+            self.URLs["download_ocn"] = f"https://{self.user}:{self.pwd}@scihub.copernicus.eu/dhus/odata/v1/Products('{self.ocn_shid}')/%24value"
 
         self.s3 = {
             "bucket" : f"s3://sentinel-s1-l1c/{sns_msg['path']}/",
         }
         if 'VV' in xml_get(self.grd.get('str'), 'polarisationmode'):
+            self.isvv = True
             swath = xml_get(self.grd.get('str'), 'swathidentifier')
             self.s3["grd_tiff"] = f"{self.s3['bucket']}measurement/{swath.lower()}-vv.tiff"
-            self.download_strs["download_grd_tiff"] = f'aws s3 cp {self.s3["grd_tiff"]} {self.dir}/vv_grd.tiff --request-payer'
+            self.s3["grd_tiff_download_str"] = f'aws s3 cp {self.s3["grd_tiff"]} {self.dir}/vv_grd.tiff --request-payer'
 
     def __repr__(self):
         return f"<SciHubObject: {self.grd_id}>"
@@ -60,19 +62,26 @@ class SHO:
         if not self.ocn:
             print('ERROR No OCN found for this GRD')
         else:
-            os.system(self.download_strs.get("download_ocn"))
+            if not os.path.exists(self.dir):
+                os.mkdir(self.dir)
+            with requests.Session() as s:
+                p = s.get(self.URLs.get("download_ocn"))
+                open(f'{self.dir}/ocn.zip', 'wb').write(p.content)            
     
     def download_grd_tiff(self): 
-        if not (self.grd and self.download_strs.get("download_grd_tiff")): 
+        if not (self.grd and self.s3["grd_tiff"]): 
             print('ERROR No grd tiff found with VV polarization')
         else:
-            os.system(self.download_strs.get("download_grd_tiff"))
+            if not os.path.exists(self.dir):
+                os.mkdir(self.dir)
+            os.system(self.s3["grd_tiff_download_str"])
 
     def update_intersection(self, ocean_shape):
         scene_poly = sh.polygon.Polygon(self.sns_msg['footprint']['coordinates'][0][0])
         self.isoceanic = scene_poly.intersects(ocean_shape)
         inter = scene_poly.intersection(ocean_shape)
         self.oceanintersection = {k: sh.mapping(inter).get(k, v) for k, v in self.sns_msg['footprint'].items()} # use msg[footprint] projection, and overwrite the intersection on top of the previous coordinates
+        self.ready = self.isoceanic and self.isvv
 
     def grd_db_row(self):
         if self.grd:
