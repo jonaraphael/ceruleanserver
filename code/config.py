15a18126512d9173ae22bdb06ddf0bf189384338
Ubuntu <ubuntu@ip-10-0-0-70.eu-central-1.compute.internal>
2020-04-13 04:52:21 +0000

    Add requirements.txt, update to new VPC

--- code/config.py
+++ code/config.py
@@ -4,16 +4,16 @@ APP_HOST = '0.0.0.0'
 APP_PORT = 80
 DEBUG = True
     
-DB_HOST = 'slick-db.cboaxrzskot9.eu-central-1.rds.amazonaws.com' 
+DB_HOST = 'db-slick.cboaxrzskot9.eu-central-1.rds.amazonaws.com' 
 DB_USER = 'postgres'
 DB_PASSWORD = 'postgres' 
-DB_DATABASE = 'slick_db' 
+DB_DATABASE = 'db_slick' 
 DB_PORT = '5432' 
 
 SH_USER = 'jonaraph' 
 SH_PWD = 'fjjEwvMDHyJH9Fa'
 
-# override with values from a loca file only if it exists
+# override with values from a local file, only if it exists
 if importlib.util.find_spec('local_config') is not None:
     from  local_config import *
 
--- local_config.py
+++
@@ -1,5 +0,0 @@
-APP_HOST = 'localhost'
-APP_PORT = 9000
-
-SH_USER = 'jonaraph'
-SH_PWD = 'fjjEwvMDHyJH9Fa'
