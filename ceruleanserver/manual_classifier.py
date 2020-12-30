d94caf08e7aca0254130bf262cf04bf477bd75e2
Jona <jonathan.raphael@gmail.com>
2020-12-30 10:33:25 -0500

    Edited Manual Classifier

--- ceruleanserver/manual_classifier.py
+++ ceruleanserver/manual_classifier.py
@@ -1,6 +1,14 @@
 #%%
 %matplotlib inline
 
+db = "cerulean"
+current_class = None
+start_date = "2020-01-01"
+end_date = "2021-01-01"
+# Default: order by polsby-popper
+set_by_area = False # Order by absolute area instead
+set_by_linear = False # Order by polsby-popper multiplied by rectangular fill-factor
+
 from db_connection import session_scope
 from utils.s3 import sync_grds_and_vecs
 from data_objects import Slick_Ext, Grd_Ext, Inference_Ext, Posi_Poly_Ext
@@ -15,39 +23,64 @@ from osgeo import gdal
 from datetime import datetime, date
 import csv
 from geoalchemy2.shape import to_shape
-from geoalchemy2 import Geography
+from geoalchemy2 import Geography, Geometry
 import shutil
 from IPython.display import clear_output
 import matplotlib.patches as patches
 
-db = "cerulean"
-set_by_area = False
-current_class = 1
-
 processed_pids = []
 outpath = Path(path_config.LOCAL_DIR) / "temp" / "outputs"
 class_int_dict = {
-    "n": 0,
-    "v": 1,
-    "f": 2,
-    "a": 3,
+    "n": 0, # Not Oil
+    "v": 1, # Vessel
+    "f": 2, # Fixed Source
+    "a": 3, # Ambiguous
     " ": None, # Skip
     "q": None, # Save and Quit
+
+    # Expert Review
+    "4": 4, # Vessel: Old
+    "18": 18, # Vessel: Recent
+    "19": 19, # Vessel: Adjacent
+    "6": 6, # Infrastructure
+    "7": 7, # Shipwreck
+    "8": 8, # Anchorage
+    "9": 9, # Natural Seep
+    "10": 10, # Wind
+    "11": 11, # Precipitation
+    "12": 12, # Suspended Particles
+    "13": 13, # Organic
+    "14": 14, # Wake
+    "15": 15, # Waves
+    "16": 16, # Unknown
+    "17": 17, # Land
+    "20": 20, # Duplicate GRD
 }
 ocean_path = Path(path_config.LOCAL_DIR) / "aux_files" / "OceanGeoJSON_lowres.geojson"
 ocean = geopandas.read_file(ocean_path)
 
 
-def get_unclassified_polys(sess, by_area=False):
+def get_unclassified_polys(sess, by_area, by_linear, start, end):
     q = (
         sess.query(Grd_Ext)
         .join(Inference_Ext)
         .join(Posi_Poly_Ext)
         .join(Slick_Ext)
         .filter(Slick_Ext.class_int == current_class)
+        .filter(Grd_Ext.starttime >= start)
+        .filter(Grd_Ext.starttime < end)
     )
     if by_area:
         q = q.order_by(desc(func.ST_Area(Posi_Poly_Ext.geometry)))
+    elif by_linear:
+        q = q.order_by(
+            desc(
+                1
+                / func.ST_Area(cast(func.ST_OrientedEnvelope(cast(Posi_Poly_Ext.geometry, Geometry)), Geography))
+                * func.ST_Perimeter(Posi_Poly_Ext.geometry)
+                * func.ST_Perimeter(Posi_Poly_Ext.geometry)
+            )
+        )
     else:
         q = q.order_by(
             desc(
@@ -101,12 +134,13 @@ def rast_to_patch(rast):
 
 
 with session_scope(commit=True, database=db, echo=False) as sess:
-    grd_list = get_unclassified_polys(sess, set_by_area)
+    grd_list = get_unclassified_polys(sess, set_by_area, set_by_linear, start_date, end_date)
+    print("Found", len(grd_list), "GRDs")
     print(f"Loading first GRD")
     download_grds([grd_list[0]], separate_process=False)
 
     for i, grd in enumerate(grd_list):
-        if i < len(grd_list):
+        if i < len(grd_list)-1:
             print("Loading next GRD in background")
             download_grds([grd_list[i + 1]], separate_process=True)
 
@@ -136,22 +170,13 @@ with session_scope(commit=True, database=db, echo=False) as sess:
         plot_super(rast=rast, vect=vect)
         plot_super(rast=rast)
         plot_super(rast=rast, vect=vect, vect_line=.2, box_factor=.25, zoom=zoom_level)
+        plot_super(rast=rast, vect=vect, vect_line=.2, box_factor=1.25, zoom=zoom_level-1)
         print(grd.pid)
         # print(grd.calc_eezs())
 
         category = "No Category Assigned"
         while category.lower() not in class_int_dict:
-            category = input("""
-                What class is this polygon? . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\
-                V = Vessel <><><> \
-                F = Fixed (Infrastructure or Natural) <><><> \
-                N = No Oil <><><> \
-                A = Ambiguous <><><> \
-                I = Zoom IN <><><> \
-                O = Zoom OUT <><><> \
-                [Blank] = Skip <><><> \
-                Q = Quit and Save Progress
-            """)
+            category = input("""Class: (V)essel, (F)ixed, (A)mbiguous, (N)one || Zoom: (I)nset, (O)utset || (Q)uit and save . . . . . . . .""")
             if category.lower() == "i":
                 zoom_level += 1
                 plot_super(rast=rast, vect=vect, vect_line=.1, box_factor=1, zoom=zoom_level)
@@ -165,11 +190,11 @@ with session_scope(commit=True, database=db, echo=False) as sess:
             slick.class_int = class_int_dict[category.lower()]
             processed_pids += [[grd.id, grd.pid]]
 
+shutil.rmtree(raster_path.parent)
+shutil.rmtree(outpath / "vectors")
+
 with open(f"{path_config.LOCAL_DIR}temp/outputs/manually_classified.csv", "a") as f:
     writer = csv.writer(f)
     for proc in processed_pids:
         writer.writerow(proc + [date.today()])
-
-shutil.rmtree(raster_path.parent)
-shutil.rmtree(outpath / "vectors")
 # %%
